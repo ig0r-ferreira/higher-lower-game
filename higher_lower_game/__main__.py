1ec1c9e866720b32bb15666348a6b3bd7304b253
Igor Ferreira <iferreira.n01@gmail.com>
2023-01-11 20:24:10 -0300

    Use rich library to customize display in terminal

--- higher_lower_game/__main__.py
+++ higher_lower_game/__main__.py
@@ -1,83 +1,36 @@
 import copy
 import random
-from textwrap import dedent
 from typing import Any
 
-from higher_lower_game import art, game_data
-
-
-def clear_console() -> None:
-    print('\033[H\033[J', end='')
-
-
-def show_logo() -> None:
-    """
-    Clear the console and show the logo.
-    """
-    clear_console()
-    print(
-        dedent(
-            """
-        {logo}
-        Welcome to Higher Lower Gamer
-    """
-        ).format(logo=art.LOGO)
-    )
-
-
-def show_comparison(profiles: dict[str, dict[str, Any]]) -> None:
-    """
-    Shows a formatted view of the reported profiles.
-    """
-    print(
-        dedent(
-            """
-        Compare A: {A[name]}, a {A[description]}, from {A[country]}
-        {versus}
-        Against B: {B[name]}, a {B[description]}, from {B[country]}
-    """
-        ).format(**profiles, versus=art.VS)
-    )
+from higher_lower_game import game_data
+from higher_lower_game.cli import CLI
 
 
 def random_profile(profile_list: list[dict[str, Any]]) -> dict[str, Any]:
-    """
-    Returns a random profile removed from the profile list.
-    """
+    """Return a random profile removed from the profile list."""
     profile = random.choice(profile_list)
     profile_list.remove(profile)
 
     return profile
 
 
-def ask_for_guess(profiles: dict[str, dict[str, Any]]) -> str:
-    """
-    Asks for a guess and returns it if it is within the allowed options.
-    """
-    while True:
-        guess = input('Who has more followers? A or B? ').upper()
-        if guess in profiles.keys():
-            return guess
-
-        print('Error: You must choose A or B.')
-
-
 def get_higher_profile(**profiles: dict[str, Any]) -> dict[str, Any]:
-    """
-    Returns the most followed profile.
-    """
+    """Return the most followed profile."""
     return max(
         profiles.values(), key=lambda profile: profile['follower_count']
     )
 
 
 def main() -> None:
+    cli = CLI()
+
     profiles_data = copy.deepcopy(game_data.DATA)
 
-    show_logo()
+    cli.clear_console()
+    cli.display_logo()
 
     if len(profiles_data) < 2:
-        print('Not enough profiles.')
+        cli.display_not_enough_profiles_error()
         return
 
     profiles = {
@@ -86,30 +39,26 @@ def main() -> None:
     }
 
     score = 0
-
     while True:
-        show_comparison(profiles)
+        cli.display_profile_comparison(profiles)
 
-        guess = ask_for_guess(profiles)
+        choice = cli.ask_user_choice()
 
-        clear_console()
-        show_logo()
+        cli.clear_console()
+        cli.display_logo()
 
         higher_profile = get_higher_profile(**profiles)
 
-        if profiles[guess] != higher_profile:
-            print(f"Sorry, that's wrong. Final score: {score}.")
+        if profiles[choice] != higher_profile:
+            cli.display_player_error(score)
             break
 
         score += 1
 
         if len(profiles_data):
-            print(f"You're right. Current score: {score}.")
+            cli.display_player_hit(score)
         else:
-            print(
-                'Congratulations, you got them all right. '
-                f'Final score: {score}.'
-            )
+            cli.display_game_complete(score)
             break
 
         profiles.update(
@@ -117,4 +66,5 @@ def main() -> None:
         )
 
 
-main()
+if __name__ == '__main__':
+    main()
---
+++ higher_lower_game/cli.py
@@ -0,0 +1,128 @@
+from typing import Any
+
+from rich.align import Align
+from rich.console import Console, RenderableType
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.prompt import Prompt
+from rich.text import Text
+
+from higher_lower_game import art
+
+
+def _format_profile_display(profile: dict[str, Any], style: str = '') -> Text:
+    """Return a text formatted with profile data."""
+    return Text(
+        '{name}, a {description}, from {country}'.format(**profile),
+        style=style,
+        justify='center',
+    )
+
+
+def _center_element(element: RenderableType, orient: str = 'both') -> Align:
+    """
+    Return a center-aligned element.
+
+    Args:
+        element (RenderableType): The renderable element that will be centered.
+        orient (str, optional): Orientation optional. One of "both", "vertical"
+        or "horizontal". Default is "both".
+    Returns:
+        Align: A center-aligned element.
+    """
+    match orient:
+        case 'both':
+            return Align(element, align='center', vertical='middle')
+        case 'horizontal':
+            return Align(element, align='center')
+        case 'vertical':
+            return Align(element, vertical='middle')
+        case _:
+            raise ValueError("Invalid option for 'orient' param.")
+
+
+def _generate_profile_panel(
+    id: str, data: dict[str, Any], border_style: str = 'none'
+) -> Panel:
+    """Return a panel whose profile data is centered."""
+    return Panel(
+        _center_element(_format_profile_display(data)),
+        title=f'Profile {id}',
+        border_style=border_style,
+    )
+
+
+class CLI:
+    """A class used to represent a CLI."""
+
+    def __init__(self) -> None:
+        self.console = Console()
+        self.width = 100
+
+    def clear_console(self) -> None:
+        """Clear console."""
+        self.console.clear()
+
+    def display_logo(self) -> None:
+        """Display LOGO."""
+        self.console.print(
+            _center_element(Text(art.LOGO, style='#FF8A00')), width=self.width
+        )
+
+    def display_not_enough_profiles_error(self) -> None:
+        """Display not enough profiles error."""
+        self.console.print(
+            'Error: Not enough profiles.\n',
+            style='bright_red',
+            width=self.width,
+        )
+
+    def display_profile_comparison(
+        self, profiles: dict[str, dict[str, Any]]
+    ) -> None:
+        """Display profile comparison."""
+        layout = Layout()
+        layout.split_row(
+            _generate_profile_panel('A', profiles['A'], '#0072F9'),
+            _center_element(Text(art.VS, style='#FFD700')),
+            _generate_profile_panel('B', profiles['B'], '#FF2000'),
+        )
+
+        self.console.print(layout, height=10, width=self.width)
+
+    def display_player_hit(self, score: int) -> None:
+        """Show the player that he got it right."""
+        self.console.print(
+            f"You're right. Current score: {score}.\n",
+            style='bright_green',
+            width=self.width,
+            justify='center',
+        )
+
+    def display_player_error(self, score: int) -> None:
+        """Show the player that he made a mistake."""
+        self.console.print(
+            f"Sorry, that's wrong. Final score: {score}.\n",
+            style='bright_red',
+            width=self.width,
+            justify='center',
+        )
+
+    def ask_user_choice(self) -> str:
+        """Ask the user which option he wants from the available ones."""
+        guess = Prompt.ask(
+            '\nWho has more followers? [#FF8A00][A/B][/]',
+            choices=['a', 'A', 'b', 'B'],
+            show_choices=False,
+        ).upper()
+        return guess
+
+    def display_game_complete(self, score: int) -> None:
+        """Show the player that he has completed the game."""
+        self.console.print(
+            'Congratulations, you got them all right. '
+            f'Final score: {score}.\n',
+            style='bright_green',
+            width=self.width,
+            justify='center',
+        )
